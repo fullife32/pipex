e5305995e003479000e1e2ae90cec022772ee005
Eithan ASSOULINE <eassouli@e2r5p2.42.fr>
2021-09-27 15:20:19 +0200

    m

--- pipex.c
+++ pipex.c
@@ -6,14 +6,11 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 17:00:48 by eassouli          #+#    #+#             */
-/*   Updated: 2021/09/24 18:47:50 by eassouli         ###   ########.fr       */
+/*   Updated: 2021/09/27 15:19:46 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "pipex.h"
 
 void	create_pipe(int pipefd[])
 {
@@ -24,7 +21,7 @@ void	create_pipe(int pipefd[])
 	}
 }
 
-int	file_check(char *path, int flag)
+int	file_check(char *path, int flag, int std)
 {
 	int	fd;
 	
@@ -35,37 +32,76 @@ int	file_check(char *path, int flag)
 		perror(path);
 		return (-1);
 	}
-	if (dup2(0, fd) == -1)
-		perror("dup2");
-	return (0);
+	if (dup2(fd, std) == -1)
+		perror("dup2"); // exit ?
+	return (fd);
 }
 
 int	main(int ac, char **av, char **env)
 {
-	int	pipefd[2];
-	int	filefd[2];
-	int	pid;
-	int	infile;
-	int	outfile;
+	int		pipe_fd[2];
+	int		file_fd[2];
+	char	**argv_tab;
+	int		pid;
+	int		infile;
+	int		outfile;
+	int		nb;
 
 	if (ac < 5)
 	{
 		write(1, "pipex: Not enough arguments\n", 28);
 		return (-1);
 	}
-	create_pipe(pipefd);
+	nb = 3;
+	argv_tab = NULL;
+	create_pipe(pipe_fd);
 	pid = fork();
 	if (pid == -1)
 		perror("fork");
 	if (pid == 0)
 	{
-		if (dup2(1, pipefd[1]) == -1)
+		close (pipe_fd[0]);
+		if (dup2(pipe_fd[1], 1) == -1)
 			perror("dup2"); //exit ?
-		filefd[0] = file_check(av[1], O_RDWR);
-		if (filefd[0] == -1)
+		file_fd[0] = file_check(av[1], O_RDONLY, 0); //ac - 1 pour le dernier file check
+		if (file_fd[0] == -1)
 			return (-1); //exit ?
-		printf("%s\n", av[2]);
-		execve("/bin/cat", &av[2], env);
+		// write(1, "test", 4);
+		argv_tab = ft_split(av[2], ' ');
+		if (argv_tab == NULL)
+			perror ("ft_split"); //free
+		if (execve("/bin/cat", argv_tab, env) == -1)
+			perror ("execve"); //free
+		close (pipe_fd[1]);
+	}
+	else
+	{
+		pid = fork();
+		if (pid == -1)
+			perror("fork");
+		if (pid == 0)
+		{
+			nb++;
+			printf("%d", nb);
+			close(pipe_fd[1]);
+			if (dup2(pipe_fd[0], 0) == -1)
+				perror("dup2"); //exit ?
+			if (nb == ac - 1)
+				file_fd[0] = file_check(av[1], O_WRONLY, 1); //ac - 1 pour le dernier file check
+			if (file_fd[0] == -1)
+				return (-1); //exit ?
+			argv_tab = ft_split(av[3], ' ');
+			if (argv_tab == NULL)
+				perror ("ft_split"); //free
+			if (execve("/bin/cat", argv_tab, env) == -1)
+				perror ("execve"); //free;
+			close (pipe_fd[0]);
+		}
+		else
+		{
+			write(1, "fini", 4);
+		}
+		
 	}
 	return (0);
 }
---
+++ pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/27 12:17:08 by eassouli          #+#    #+#             */
+/*   Updated: 2021/09/27 12:42:19 by eassouli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	PIPEX_H
+#define	PIPEX_H
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char	**ft_split(char const *s, char c);
+
+#endif
