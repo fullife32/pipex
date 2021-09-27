a18b39b6b9fa42c596cc200c0a2c38c5f98f10a2
Eithan ASSOULINE <eassouli@e2r5p2.42.fr>
2021-09-27 18:01:56 +0200

    add file_check

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 17:00:48 by eassouli          #+#    #+#             */
-/*   Updated: 2021/09/27 16:37:00 by eassouli         ###   ########.fr       */
+/*   Updated: 2021/09/27 18:01:18 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,34 +21,42 @@ void	create_pipe(int pipefd[])
 	}
 }
 
-int	file_check(char *path, int std)
+void	file_check(char *path1, char *path2, int file_fd[2])
 {
-	int	fd;
-	
-	if (std == OUT)
-		fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	else
-		fd = open(path, O_RDONLY);
-	if (fd == -1)
+	file_fd[0] = open(path1, O_RDONLY);
+	if (file_fd[0] == -1)
 	{
-		perror(path);
-		return (-1);
+		perror(path1);
+		exit(EXIT_FAILURE);
 	}
-	if (dup2(fd, std) == -1)
-		perror("dup2"); // exit ?
-	return (fd);
+	file_fd[1] = open(path2, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (file_fd[1] == -1)
+	{
+		perror(path2);
+		close(file_fd[0]);
+		exit(EXIT_FAILURE);
+	}
+}
+
+char	*access_path(char **env)
+{
+	while (env || ft_strncmp(*env, "PATH=", 5) != 0)
+		env++;
+	if (env == NULL)
+	{
+		write(1, "pipex : No path for command found\n", 34); // A changer
+		return (NULL);
+	}
 }
 
 int	main(int ac, char **av, char **env)
 {
-	char	*access_path;
+	char	*cmd_path;
 	int		pipe_fd[2];
 	int		file_fd[2];
 	char	**argv_tab;
 	char	**argv_tab2;
 	int		pid;
-	int		infile;
-	int		outfile;
 	int		nb;
 
 	if (ac < 5)
@@ -63,29 +71,26 @@ int	main(int ac, char **av, char **env)
 	nb = 3;
 	argv_tab = NULL;
 	argv_tab2 = NULL;
-	
 	create_pipe(pipe_fd);
+	file_check(av[1], av[ac - 1], file_fd);
+	// cmd_path = access_path(env);
 	pid = fork();
 	if (pid == -1)
 		perror("fork");
 	if (pid == 0)
 	{
-		close (pipe_fd[0]);
-		if (dup2(pipe_fd[1], 1) == -1)
+		close(pipe_fd[IN]);
+		if (dup2(file_fd[IN], IN) == -1)
 			perror("dup2"); //exit ?
-		file_fd[0] = file_check(av[1], 0); //ac - 1 pour le dernier file check
-		if (file_fd[0] == -1)
-			return (-1); //exit ?
-		// write(1, "test", 4);
+		if (dup2(pipe_fd[OUT], OUT) == -1)
+			perror("dup2"); //exit ?
 		argv_tab = ft_split(av[2], ' ');
 		if (argv_tab == NULL)
 			perror ("ft_split"); //free
-		char	**env_tab;
-		ft_strncmp()
 		if (execve("/bin/cat", argv_tab, env) == -1)
 			perror ("execve"); //free
-		close (pipe_fd[1]);
-		exit(0);
+		close(pipe_fd[OUT]);
+		exit(EXIT_SUCCESS);
 	}
 	else
 	{
@@ -94,20 +99,17 @@ int	main(int ac, char **av, char **env)
 			perror("fork");
 		if (pid == 0)
 		{
-			close (pipe_fd[1]);
-			nb++;
-			if (dup2(pipe_fd[0], 0) == -1)
+			close(pipe_fd[OUT]);
+			if (dup2(pipe_fd[IN], IN) == -1)
 				perror("dup2"); //exit ?
-			if (nb == ac - 1)
-				file_fd[0] = file_check(av[4], 1); //ac - 1 pour le dernier file check
-			if (file_fd[0] == -1)
-				return (-1); //exit ?
+			if (dup2(file_fd[OUT], OUT) == -1)
+				perror("dup2"); // exit ?
 			argv_tab2 = ft_split(av[3], ' ');
 			if (argv_tab2 == NULL)
 				perror ("ft_split"); //free
 			if (execve("/bin/cat", argv_tab2, env) == -1)
 				perror ("execve"); //free;
-			close (pipe_fd[0]);
+			close(pipe_fd[IN]);
 			exit(0);
 		}
 	}
