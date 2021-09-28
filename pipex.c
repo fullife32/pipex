68978598d9a03d1953485777628cce7e44746adf
Eithan ASSOULINE <eassouli@e1r10p1.42.fr>
2021-09-28 15:22:48 +0200

    fix liste chainee

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 17:00:48 by eassouli          #+#    #+#             */
-/*   Updated: 2021/09/28 12:43:22 by eassouli         ###   ########.fr       */
+/*   Updated: 2021/09/28 15:21:09 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,13 @@ void	file_check(char *path1, char *path2, int file_fd[2])
 	}
 }
 
-char	*access_path(int ac, char **av, char **env, t_list **lst)
+t_list	*access_path(int ac, char **av, char **env, t_list **lst)
 {
 	char	**split_path;
 	char	**split_args;
 	int		arg;
 	int		i;
+	t_list	*list;
 
 	split_path = NULL;
 	while (*env && ft_strncmp(*env, "PATH=", 5) != 0)
@@ -56,50 +57,58 @@ char	*access_path(int ac, char **av, char **env, t_list **lst)
 	split_path = ft_split(*env, ':');
 	if (split_path == NULL)
 		return (NULL); // exit
-	*split_path += 5;
 	arg = 2;
+	*split_path += 5;
+	list = NULL;
 	while (arg < ac - 1)
 	{
-		*lst = ft_lstnew(NULL);
-		if (*lst == NULL)
+		if (list)
+		{
+			list->next = ft_lstnew(NULL);
+			list = list->next;
+		}
+		else
+		{
+			*lst = ft_lstnew(NULL);
+			list = *lst;
+		}
+		if (list == NULL)
 			return (NULL); //clear lst
-		(*lst)->split_args = ft_split(av[arg], ' '); //dup dans liste chainee
-		if ((*lst)->split_args == NULL)
+		list->split_args = ft_split(av[arg], ' '); //dup dans liste chainee
+		if (list->split_args == NULL)
 			return (NULL); //exit
-		if (**(*lst)->split_args == '/')
+		if (list->split_args[0][0] == '/')
 		{
 			if (access(*split_args, X_OK) == 0)
 			{
-				(*lst)->path = *(*lst)->split_args; //strdup, check quand meme access quand exec commande au cas ou ne marche pas
-				printf("Le bon chemin est : %s\n", (*lst)->path);
+				list->path = *(list->split_args); //strdup, check quand meme access quand exec commande au cas ou ne marche pas
+				printf("Le bon chemin est : %s\n", list->path);
 			}
 		}
 		else
 		{
-			*(*lst)->split_args = ft_strjoin("/", *(*lst)->split_args);
-			if (*(*lst)->split_args == NULL)
+			*(list->split_args) = ft_strjoin("/", *(list->split_args));
+			if (*(list->split_args) == NULL)
 				return (NULL);
 			i = 0;
 			while (split_path[i] && i != -1)
 			{
-				(*lst)->path = ft_strjoin(split_path[i], *(*lst)->split_args);
-				if ((*lst)->path == NULL)
+				list->path = ft_strjoin(split_path[i], *(list->split_args));
+				if (list->path == NULL)
 					return (NULL);
-				if (access((*lst)->path, X_OK) == 0)
+				if (access(list->path, X_OK) == 0)
 					i = -2;
 				else
 				{
-					free((*lst)->path);
-					(*lst)->path = NULL;
+					free(list->path);
+					list->path = NULL;
 				}
 				i++;
 			}
 		}
 		arg++;
-		(*lst)->next = ft_lstnew(NULL);
-		*lst = (*lst)->next;
 	}
-	return (NULL);
+	return (*lst);
 }
 
 int	main(int ac, char **av, char **env)
@@ -110,7 +119,7 @@ int	main(int ac, char **av, char **env)
 	char	**argv_tab2;
 	int		pid;
 	int		nb;
-	t_list	**lst;
+	t_list	*lst;
 
 	if (ac < 5)
 	{
@@ -124,11 +133,12 @@ int	main(int ac, char **av, char **env)
 	nb = 3;
 	argv_tab = NULL;
 	argv_tab2 = NULL;
-	*lst = NULL;
+	lst = NULL;
 	create_pipe(pipe_fd);
 	file_check(av[1], av[ac - 1], file_fd);
-	access_path(ac, av, env, lst);
-	// printf("Le bon chemin est : %s\n", (*lst)->path);
+	lst = access_path(ac, av, env, &lst);
+	write(1, lst->path, 5);
+	// dprintf(2, "Le bon chemin est : %s\n", lst->path);
 	pid = fork();
 	if (pid == -1)
 		perror("fork");
@@ -139,13 +149,15 @@ int	main(int ac, char **av, char **env)
 			perror("dup2"); //exit ?
 		if (dup2(pipe_fd[OUT], OUT) == -1)
 			perror("dup2"); //exit ?
-		if (execve((*lst)->path, (*lst)->split_args, env) == -1)
+		if (execve(lst->path, lst->split_args, env) == -1)
 			perror ("execve"); //free
 		close(pipe_fd[OUT]);
 		exit(EXIT_SUCCESS);
 	}
 	else
 	{
+		dprintf(2, "%p\n", lst->next);
+		lst = lst->next;
 		pid = fork();
 		if (pid == -1)
 			perror("fork");
@@ -156,10 +168,7 @@ int	main(int ac, char **av, char **env)
 				perror("dup2"); //exit ?
 			if (dup2(file_fd[OUT], OUT) == -1)
 				perror("dup2"); // exit ?
-			argv_tab2 = ft_split(av[3], ' ');
-			if (argv_tab2 == NULL)
-				perror ("ft_split"); //free
-			if (execve("/bin/cat", argv_tab2, env) == -1)
+			if (execve(lst->path, lst->split_args, env) == -1)
 				perror ("execve"); //free;
 			close(pipe_fd[IN]);
 			exit(0);
