5dc6cfb621ea2e5b98ad32e83ecf900432b37bcb
Eithan ASSOULINE <eassouli@e2r9p4.42.fr>
2021-10-01 18:39:55 +0200

    leaks

--- srcs/create_list.c
+++ srcs/create_list.c
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 16:24:13 by eassouli          #+#    #+#             */
-/*   Updated: 2021/10/01 17:57:21 by eassouli         ###   ########.fr       */
+/*   Updated: 2021/10/01 18:39:27 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,6 +70,22 @@ int	split_args(int arg, char **av, t_list *list, t_pipex *pipex)
 	return (0);
 }
 
+t_list *create_node(int arg, char **av, t_pipex *pipex)
+{
+	t_list	*list;
+
+	list = ft_lstnew(NULL);
+	if (list == NULL)
+		return (NULL);
+	if (create_pipe(list->pipe_fd) == -1)
+		return (NULL);
+	if (arg == 2 && pipex->file_fd[IN] == -1)
+		list->fail = 1;
+	if (split_args(arg, av, list, pipex) == -1)
+		return (NULL);
+	return (list);
+}
+
 int	create_list(int ac, char **av, t_list **lst, t_pipex *pipex)
 {
 	int		arg;
@@ -105,3 +121,47 @@ int	create_list(int ac, char **av, t_list **lst, t_pipex *pipex)
 	}
 	return (0);
 }
+
+// dprintf(2, "pipefd : %d %d list->path ; %s list-> argv[0] %s\n", list->pipe_fd[0], list->pipe_fd[1], list->path, list->args[0]);
+
+// t_list *create_node(int arg, char **av, t_pipex *pipex)
+// {
+// 	t_list	*list;
+
+// 	list = ft_lstnew(NULL);
+// 	if (list == NULL)
+// 		return (NULL);
+// 	if (create_pipe(list->pipe_fd) == -1)
+// 		return (NULL);
+// 	if (arg == 2 && pipex->file_fd[IN] == -1)
+// 		list->fail = 1;
+// 	if (split_args(arg, av, list, pipex) == -1)
+// 		return (NULL);
+// 	return (list);
+// }
+
+// int	create_list(int ac, char **av, t_list **lst, t_pipex *pipex)
+// {
+// 	int		arg;
+// 	t_list	*list;
+
+// 	if (split_env(pipex) == -1)
+// 		return (-1); //free a la fin
+// 	if (pipex->env_path == NULL)
+// 		return (-1);
+// 	arg = 2;
+// 	list = create_node(arg, av, pipex);
+// 	if (list == NULL)
+// 		return (-1);
+// 	*lst = list;
+// 	arg++;
+// 	while (arg < ac - 1)
+// 	{
+// 		list->next = create_node(arg, av, pipex);
+// 		if (list->next == NULL)
+// 			return (-1);
+// 		list = list->next;
+// 		arg++;
+// 	}
+// 	return (0);
+// }
