aaadeb801821811e9dd4df70f5e3e45e141fdc3b
Eithan ASSOULINE <eassouli@e1r3p15.42.fr>
2021-10-07 19:16:50 +0200

    leaks

--- srcs/fork/exec_cmd.c
+++ srcs/fork/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 17:47:48 by eassouli          #+#    #+#             */
-/*   Updated: 2021/10/07 19:09:46 by eassouli         ###   ########.fr       */
+/*   Updated: 2021/10/07 19:16:29 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,57 @@ void	exec_cmd(t_list *lst, t_pipex *pipex)
 		(EXIT_FAILURE);
 	if (dup_stdout(lst, pipex) == -1)
 		(EXIT_FAILURE);
-	if (access(lst->path, X_OK) == 0)
+	if (execve(lst->path, lst->args, pipex->env) == -1)
 	{
-		if (execve(lst->path, lst->args, pipex->env) == -1)
+		if (access(lst->path, X_OK) == -1)
 		{
 			perror("execve");
 			if (lst->prev)
 				close(lst->prev->pipe_fd[IN]);
 			close(lst->pipe_fd[OUT]);
-			exit(1);
+			exit(errno);
+		}
+		else
+		{
+			close(lst->pipe_fd[IN]);
+			close(lst->pipe_fd[OUT]);
+			while (lst->prev)
+				lst = lst->prev;
+			free_pipex(&lst, pipex);
+			exit(127);
 		}
 	}
-	else
-	{
-		close(lst->pipe_fd[IN]);
-		close(lst->pipe_fd[OUT]);
-		while (lst->prev)
-			lst = lst->prev;
-		free_pipex(&lst, pipex);
-		exit(127);
-	}
 }
+
+
+// void	exec_cmd(t_list *lst, t_pipex *pipex)
+// {
+// 	if (dup_stdin(lst, pipex) == -1)
+// 		(EXIT_FAILURE);
+// 	if (dup_stdout(lst, pipex) == -1)
+// 		(EXIT_FAILURE);
+// 	if (access(lst->path, X_OK) == 0)
+// 	{
+// 		if (execve(lst->path, lst->args, pipex->env) == -1)
+// 		{
+// 			perror("execve");
+// 			if (lst->prev)
+// 				close(lst->prev->pipe_fd[IN]);
+// 			close(lst->pipe_fd[OUT]);
+// 			exit(errno);
+// 		}
+// 	}
+// 	else if (access(lst->path, X_OK) == -1)
+// 	{
+// 		exit(125);
+// 	}
+// 	else
+// 	{
+// 		close(lst->pipe_fd[IN]);
+// 		close(lst->pipe_fd[OUT]);
+// 		while (lst->prev)
+// 			lst = lst->prev;
+// 		free_pipex(&lst, pipex);
+// 		exit(127);
+// 	}
+// }
