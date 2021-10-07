e156f06f6f09fd717b88ec70db04ee37eb936116
Eithan ASSOULINE <eassouli@e1r3p15.42.fr>
2021-10-07 19:09:48 +0200

    leaks

--- srcs/fork/exec_cmd.c
+++ srcs/fork/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 17:47:48 by eassouli          #+#    #+#             */
-/*   Updated: 2021/10/07 19:08:23 by eassouli         ###   ########.fr       */
+/*   Updated: 2021/10/07 19:09:46 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	exec_cmd(t_list *lst, t_pipex *pipex)
 			if (lst->prev)
 				close(lst->prev->pipe_fd[IN]);
 			close(lst->pipe_fd[OUT]);
-			exit(127);
+			exit(1);
 		}
 	}
 	else
