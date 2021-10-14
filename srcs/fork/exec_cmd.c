5690572c1a7526cd8f4fe7d05861d3c625cdd1de
Eithan ASSOULINE <eassouli@e1r1p10.42.fr>
2021-10-14 15:32:40 +0200

    norm and fix out

--- srcs/fork/exec_cmd.c
+++ srcs/fork/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 17:47:48 by eassouli          #+#    #+#             */
-/*   Updated: 2021/10/07 19:45:39 by eassouli         ###   ########.fr       */
+/*   Updated: 2021/10/14 14:57:52 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,22 @@ void	exec_cmd(t_list *lst, t_pipex *pipex)
 		(EXIT_FAILURE);
 	if (dup_stdout(lst, pipex) == -1)
 		(EXIT_FAILURE);
+	if (access(lst->path, X_OK) == -1)
+	{
+		close(lst->pipe_fd[IN]);
+		close(lst->pipe_fd[OUT]);
+		while (lst->prev)
+			lst = lst->prev;
+		free_pipex(&lst, pipex);
+		exit(127);
+	}
 	if (execve(lst->path, lst->args, pipex->env) == -1)
 	{
-		if (access(lst->path, X_OK) == 0)
-		{
-			perror("execve");
-			if (lst->prev)
-				close(lst->prev->pipe_fd[IN]);
-			close(lst->pipe_fd[OUT]);
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			close(lst->pipe_fd[IN]);
-			close(lst->pipe_fd[OUT]);
-			while (lst->prev)
-				lst = lst->prev;
-			free_pipex(&lst, pipex);
-			exit(127);
-		}
+		perror("execve");
+		if (lst->prev)
+			close(lst->prev->pipe_fd[IN]);
+		close(lst->pipe_fd[OUT]);
+		exit(EXIT_FAILURE);
 	}
 }
 
